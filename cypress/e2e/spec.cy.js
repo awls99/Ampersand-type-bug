d3777da208b3348f6d2dce81c7162db7a605864f
Arthur <arthur@iofinnet.com>
2024-03-12 14:17:27 +0000

    bug demo

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,7 @@
 /// <reference types="cypress" />
 describe('page', () => {
   it('works', () => {
-    cy.visit('https://example.cypress.io')
+    cy.log('This should log and amperstand html entity: &amp;');
+    cy.log('This should log plain ampersand: &');
   })
 })
